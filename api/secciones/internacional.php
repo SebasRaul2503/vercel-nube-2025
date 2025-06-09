9dbd0854d45f7e3d99eaf3b64f356bb73083f485
Sebastian Castillo <145416890+SebasRaul2503@users.noreply.github.com>
2025-06-09 12:14:03 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Negociaciones comerciales entre EEUU y China giran en torno a materiales criticos",
+"autor" => "Sebastian Castillo",
+"resumen" => "Las tensiones comerciales entre EEUU y China se intensificaron este año cuando Trump aumento los aranceles sobre productos chinos lo que provoco represalias por parte de Pekin.",
 ];
 ?>
