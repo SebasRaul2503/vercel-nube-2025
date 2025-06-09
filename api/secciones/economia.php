2d1e06a03e16015b3b7eb301fce3d9c409adaec5
Sebastian Castillo <145416890+SebasRaul2503@users.noreply.github.com>
2025-06-09 12:13:36 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La economía del Perú creció un poderoso 4,67% en marzo: los factores decisivos que catapultaron el PBI en el tercer mes del año",
+"autor" => "Esteban Salazar Herrada",
+"resumen" => "Doce meses en racha. Desde abril del año pasado, el país ha mantenido un ritmo constante de expansión, impulsado por el desempeño favorable de diversas actividades productivas",
 ];
 ?>
