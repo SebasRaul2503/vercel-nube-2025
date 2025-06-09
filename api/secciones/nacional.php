5e954e16e2ba8543b2d8d6b43cc4a00cc86dcb58
Sebastian Castillo <145416890+SebasRaul2503@users.noreply.github.com>
2025-06-09 12:13:50 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Pablo confiesa todo sobre su romance clandestino con Milett",
+"autor" => "Sebastian Castillo",
+"resumen" => "Nos Veiamos a escondidas",
 ];
 ?>
